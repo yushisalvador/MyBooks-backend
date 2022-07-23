e1334e7dcefa9dfa44f60b9357dd65081a96d152
yccjs <yccjsalvador@gmail.com>
2022-07-23 11:06:59 +0900

    chore: test for auth

--- test/server/routes/auth/index.spec.ts
+++ test/server/routes/auth/index.spec.ts
@@ -1,11 +1,10 @@
 import request from "supertest";
 import { expect } from "chai";
-
 import buildServer from "server";
 const app = buildServer();
 
 describe("auth routes", function () {
-  it("/auth responds with 200", function (done) {
+  it("should respond with 200", function (done) {
     request(app).get("/auth").expect(200, done);
   });
 });
