7f99d20b40031343124db145f84098dd289e94c6
yccjs <yccjsalvador@gmail.com>
2022-08-03 12:56:35 +0900

    clean up: unnecessary route

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application } from "express";
 import routes from "./routes/index";
 
 const cors = require("cors");
 
 export default function buildServer() {
   const app: Application = express();
-
-  app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello world!");
-  });
 
   app.use(cors());
 
