3616cd06874d9233e0512edd51f709f401efbcf7
yccjs <yccjsalvador@gmail.com>
2022-08-04 17:44:32 +0900

    chore: fix promise catch code.

--- src/books/books.model.ts
+++ src/books/books.model.ts
@@ -4,7 +4,7 @@ const knex = require("knex")(config);
 const booksTable = "books";
 
 const getAllBooks = () => {
-  return knex.select("*").from(booksTable).catch(console.error());
+  return knex.select("*").from(booksTable).catch(console.error);
 };
 
 const getUserBooks = (username: String) => {
@@ -12,18 +12,18 @@ const getUserBooks = (username: String) => {
     .select("*")
     .from(booksTable)
     .where("books.registered_by", username)
-    .catch(console.error());
+    .catch(console.error);
 };
 
 const addNewBook = (book: Object) => {
-  return knex.insert(book).into(booksTable).catch(console.error());
+  return knex.insert(book).into(booksTable).catch(console.error);
 };
 
 const editBook = (id: Number, update: Object) => {
   return knex(booksTable)
     .update(update)
     .where("books.id", id)
-    .catch(console.error());
+    .catch(console.error);
 };
 
 const deleteBook = (id: Number) => {
@@ -31,7 +31,7 @@ const deleteBook = (id: Number) => {
     .select("*")
     .where("books.id", id)
     .del()
-    .catch(console.error());
+    .catch(console.error);
 };
 
 const deleteUserBooks = (username: String) => {
@@ -39,7 +39,7 @@ const deleteUserBooks = (username: String) => {
     .select("*")
     .where("books.registered_by", username)
     .del()
-    .catch(console.error());
+    .catch(console.error);
 };
 
 module.exports = {
