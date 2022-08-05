7b3dcec3b83932842ad2f0091d804b4628128074
yccjs <yccjsalvador@gmail.com>
2022-08-05 09:40:55 +0900

    fix: try solving cors issue

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import routes from "./routes/index";
 
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 export default function buildServer() {
   const app = express();
 
-  app.use(cors());
+  function solveCorsIssue(req: Request, res: Response, next: any) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  }
+
+  app.use(solveCorsIssue);
+  // app.use(cors());
 
   app.use(express.json());
   app.use(routes);
