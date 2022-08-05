b9e538482c38d2f215772b7d5e17337cebce1c77
yccjs <yccjsalvador@gmail.com>
2022-08-05 09:50:07 +0900

    chore: return cors

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,12 @@ import routes from "./routes/index";
 import { Request, Response } from "express";
 
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 export default function buildServer() {
   const app = express();
 
-  function solveCorsIssue(req: Request, res: Response, next: any) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-  }
-
-  app.use(solveCorsIssue);
-  // app.use(cors());
-
+  app.use(cors());
   app.use(express.json());
   app.use(routes);
 
