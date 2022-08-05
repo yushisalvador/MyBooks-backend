b75a47aa7a7e1afc82eb108d3c5ba9264d656af7
yccjs <yccjsalvador@gmail.com>
2022-08-05 12:59:36 +0900

    chore: add frontend url to server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import routes from "./routes/index";
+import { Request, Response, NextFunction } from "express";
 
 const express = require("express");
 const cors = require("cors");
@@ -6,6 +7,20 @@ const cors = require("cors");
 export default function buildServer() {
   const app = express();
 
+  function solveCorsIssue(req: Request, res: Response, next: NextFunction) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://d317g6gtx7oo04.cloudfront.net/"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  }
+
+  app.use(solveCorsIssue);
   app.use(cors());
   app.use(express.json());
   app.use(routes);
