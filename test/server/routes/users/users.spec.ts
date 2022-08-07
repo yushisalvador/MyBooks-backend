7958834cd6d2cede8a6fc98df3e67233d1ad865a
yccjs <yccjsalvador@gmail.com>
2022-08-07 14:52:37 +0900

    chore: uncomment tests

--- test/server/routes/users/users.spec.ts
+++ test/server/routes/users/users.spec.ts
@@ -1,38 +1,38 @@
-// import request from "supertest";
-// import buildServer from "../../../../src/server";
-// import { expect } from "chai";
-// const app = buildServer();
-// const fixtures = require("../fixtures.ts");
-// import { User } from "../../../../src/types/types";
-// describe("POST User registration", () => {
-//   const newUserObj = fixtures.getUser();
-//   afterEach(async () => {
-//     await request(app).delete(`/auth?username=${newUserObj.username}`);
-//   });
+import request from "supertest";
+import buildServer from "../../../../src/server";
+import { expect } from "chai";
+const app = buildServer();
+const fixtures = require("../fixtures.ts");
+import { User } from "../../../../src/types/types";
+describe("POST User registration", () => {
+  const newUserObj = fixtures.getUser();
+  afterEach(async () => {
+    await request(app).delete(`/auth?username=${newUserObj.username}`);
+  });
 
-//   it("should return status 201 when registration is successful", async () => {
-//     const res = await request(app).post("/auth/register").send(newUserObj);
-//     expect(res.statusCode).equals(201);
-//   });
+  it("should return status 201 when registration is successful", async () => {
+    const res = await request(app).post("/auth/register").send(newUserObj);
+    expect(res.statusCode).equals(201);
+  });
 
-//   it("should store the username and password to the database", async () => {
-//     await request(app).post("/auth/register").send(newUserObj);
-//     const allUsers = await request(app).get("/auth");
-//     const newUser = await allUsers.body.find(
-//       (user: User) => user.username === newUserObj.username
-//     );
-//     expect(newUser).to.not.equal(undefined);
-//   });
+  it("should store the username and password to the database", async () => {
+    await request(app).post("/auth/register").send(newUserObj);
+    const allUsers = await request(app).get("/auth");
+    const newUser = await allUsers.body.find(
+      (user: User) => user.username === newUserObj.username
+    );
+    expect(newUser).to.not.equal(undefined);
+  });
 
-//   it("should never store an unhashed password", async () => {
-//     await request(app).post("/auth/register").send(newUserObj);
-//     const allUsers = await request(app).get("/auth");
-//     const findUser = await allUsers.body.find(
-//       (user: User) => user.username === newUserObj.username
-//     );
-//     expect(findUser.pass).to.not.equal(newUserObj.pass);
-//   });
-// });
+  it("should never store an unhashed password", async () => {
+    await request(app).post("/auth/register").send(newUserObj);
+    const allUsers = await request(app).get("/auth");
+    const findUser = await allUsers.body.find(
+      (user: User) => user.username === newUserObj.username
+    );
+    expect(findUser.pass).to.not.equal(newUserObj.pass);
+  });
+});
 
 // it("should return 401 if username or password is not given", async () => {
 //   const newObj = {
