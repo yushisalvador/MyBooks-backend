385d979577f1d2efe43b0f7275d85b58332ca5f5
yccjs <yccjsalvador@gmail.com>
2022-08-08 22:08:55 +0900

    chore: update imports

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,18 +1,18 @@
 const express = require("express");
-const userController = require("../user/user.controller");
+const authController = require("../auth/auth.controller");
 const router = express.Router();
 
 const middleware = require("../middleware/middleware");
 
-router.get("/", middleware.authenticateFunction, userController.getAllUsers);
+router.get("/", middleware.authenticateFunction, authController.getAllUsers);
 
 //POST
-router.post("/register", userController.addUser);
-router.post("/login", userController.login);
-router.post("/token", userController.getAccessToken);
+router.post("/register", authController.addUser);
+router.post("/login", authController.login);
+router.post("/token", authController.getAccessToken);
 
 //DELETE
 
-router.delete("/", userController.deleteUser);
-router.delete("/logout", userController.logout);
+router.delete("/", authController.deleteUser);
+router.delete("/logout", authController.logout);
 export default router;
