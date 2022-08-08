46d7e9c0908b71d2eafe75ee09dbd6e187a60bd7
yccjs <yccjsalvador@gmail.com>
2022-08-08 10:13:02 +0900

    chore: add redirect

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,9 @@ const cors = require("cors");
 export default function buildServer() {
   const app = express();
 
-  function solveCorsIssue(req: Request, res: Response, next: NextFunction) {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://resplendent-choux-f0a075.netlify.app/"
-    );
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-  }
-
-  app.use(solveCorsIssue);
+  app.get("/", (req: Request, res: Response) => {
+    res.redirect("https://little-world-of-books.netlify.app");
+  });
   app.use(cors());
   app.use(express.json());
   app.use(routes);
